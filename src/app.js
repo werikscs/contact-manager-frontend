46fa8ae14d7e5b1af61525f78e2b985d4c6ec2ea
Weriks Santos <werikscs@gmail.com>
2022-09-24 13:13:03 -0300

    feat: add new colors to toastify

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
+import { ToastContainer } from "react-toastify";
 import AppRoutes from "./routes";
 import { AppStyled } from "./styles/app-style";
 import { GlobalStyle } from "./styles/global";
 
 const App = () => {
   return (
-    <AppStyled>
-      <GlobalStyle />
-      <AppRoutes />
-    </AppStyled>
+    <>
+      <ToastContainer />
+      <AppStyled>
+        <GlobalStyle />
+        <AppRoutes />
+      </AppStyled>
+    </>
   );
 };
 
