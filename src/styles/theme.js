1ba1ef049315b41522e45f6b291d6f040153ebf4
Weriks Santos <werikscs@gmail.com>
2022-09-24 01:17:46 -0300

    feat: add more colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,10 +1,12 @@
 export const theme = {
   color: {
     blue1: "#3D5A80",
+    blue1_25: "rgba(55, 82, 116, 0.25)",
     blue2: "#98C1D9",
     blue3: "#E0FBFC",
     orange1: "#EE6C4D",
     black1: "#293241",
+    black1_50: "rgba(41, 50, 65, .5)",
     white1: "#ffffff",
     black_accent1: "#293241",
   },
