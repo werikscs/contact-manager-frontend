485b15b9bbbae0a237339a4340b6938141947c7b
Weriks Santos <werikscs@gmail.com>
2022-09-24 00:41:17 -0300

    style: change spaces from 4 to 2

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from "react-router-dom"
-import LoginPage from "../pages/login"
+import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "../pages/login";
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<LoginPage/>}/>
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+    </Routes>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
