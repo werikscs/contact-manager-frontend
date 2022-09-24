5b91364d0da08bfe3c81d07eb02a1f0d9e82144c
Weriks Santos <werikscs@gmail.com>
2022-09-24 15:22:02 -0300

    feat: create file to handle the framer-motion animations

---
+++ src/utils/animations-variants.js
@@ -0,0 +1,12 @@
+export const basicWrapperMotion = {
+  initial: { scale: 0.9, opacity: 0 },
+  finalized: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: 0.5,
+      type: "tween",
+    },
+  },
+};
