9b624cc9404bfbebaf37cfec882b5d511744e0c6
Weriks Santos <werikscs@gmail.com>
2022-09-24 12:04:56 -0300

    refactor: change basic-wrapper component to be more reusable

--- src/components/basic-wrapper/index.jsx
+++ src/components/basic-wrapper/index.jsx
@@ -1,9 +1,5 @@
 import { BasicWrapperStyled } from "./style";
 
 export const BasicWrapper = ({ children }) => {
-  return (
-    <BasicWrapperStyled>
-      <div>{children}</div>
-    </BasicWrapperStyled>
-  );
+  return <BasicWrapperStyled>{children}</BasicWrapperStyled>;
 };
--- src/components/basic-wrapper/style.js
+++ src/components/basic-wrapper/style.js
@@ -4,32 +4,20 @@ export const BasicWrapperStyled = styled.main`
   margin: 0 auto;
 
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  height: 100vh;
+  min-width: 360px;
+  max-width: 400px;
+
+  border-radius: 0.5rem;
+
+  background-color: ${({ theme }) => theme.color.white1};
+
+  box-shadow: 0 0.25rem 0.5rem ${({ theme }) => theme.color.blue1};
 
   h1 {
-    color: ${({ theme }) => theme.color.black1};
-  }
-
-  > div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    gap: 1rem;
-
-    background-color: ${({ theme }) => theme.color.white1};
-
-    min-width: 360px;
-    max-width: 400px;
-    height: fit-content;
-
-    border-radius: 0.5rem;
-
-    box-shadow: 0 0.25rem 0.5rem ${({ theme }) => theme.color.blue1};
-
-    padding: 1.5rem 1rem;
+    padding: 1rem;
   }
 `;
