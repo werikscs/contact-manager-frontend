ea3325996c0c02fecb7b0105783cb8eb9538cd69
Weriks Santos <werikscs@gmail.com>
2022-09-24 00:40:31 -0300

    feat: create a file to handle with the app theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { ThemeProvider } from "styled-components";
+import App from "./app";
+import { theme } from "./styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>
   </BrowserRouter>
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  color: {
+    blue1: "#3D5A80",
+    blue2: "#98C1D9",
+    blue3: "#E0FBFC",
+    orange1: "#EE6C4D",
+    black1: "#293241",
+    white1: "#ffffff",
+    black_accent1: "#293241",
+  },
+  font: "Inter, sans-serif",
+};
