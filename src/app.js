cbce72eb8529dc74b134dae311929667fd92d611
Weriks Santos <werikscs@gmail.com>
2022-09-24 00:42:41 -0300

    refactor: change the file App.js to app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import AppRoutes from "./routes";
+import { AppStyled } from "./styles/app-style";
 import { GlobalStyle } from "./styles/global";
 
 const App = () => {
   return (
-    <>
+    <AppStyled>
       <GlobalStyle />
       <AppRoutes />
-    </>
+    </AppStyled>
   );
 };
 
