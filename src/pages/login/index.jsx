adb40f250c785853d120bfb0c55e4eb38ffd0718
Weriks Santos <werikscs@gmail.com>
2022-09-24 02:16:39 -0300

    feat: create login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,31 @@
-const LoginPage = () => {
-    return <h1>Login Page</h1>
-}
+import { BasicButton } from "../../components/basic-button";
+import { BasicDivider } from "../../components/basic-divider";
+import { BasicWrapper } from "../../components/basic-wrapper";
+import { BasicInput } from "../../components/basic-input";
+import { MessageRedirect } from "../../components/msg-redirect";
 
-export default LoginPage
+export const LoginPage = () => {
+  return (
+    <BasicWrapper>
+      <h1>Entrar</h1>
+      <BasicDivider />
+      <BasicInput
+        label="Email"
+        type="text"
+        placeholder="Digite um email válido"
+      />
+      <BasicInput
+        label="Senha"
+        type="password"
+        placeholder="Digite sua senha"
+      />
+      <BasicButton text="Entrar" margin="1rem 0 0 0" />
+      <BasicDivider />
+      <MessageRedirect
+        text="Não tem conta?"
+        path="/register"
+        pathText="Cadastre-se"
+      />
+    </BasicWrapper>
+  );
+};
