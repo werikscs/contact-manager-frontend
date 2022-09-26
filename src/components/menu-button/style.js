9efc3dbbac65d0455b2bfeae5d5986cd7b77a939
Weriks Santos <werikscs@gmail.com>
2022-09-26 20:26:50 -0300

    refactor: change menu buttons style

--- src/components/menu-button/style.js
+++ src/components/menu-button/style.js
@@ -3,27 +3,29 @@ import styled from "styled-components";
 export const MenuButtonStyled = styled.button`
   width: fit-content;
 
-  padding: 0.5rem 1rem;
-
   border-radius: 0.5rem;
 
   font-size: 1rem;
   font-weight: bold;
 
-  color: ${({ theme }) => theme.color.white1};
-  background-color: ${({ theme }) => theme.color.blue1};
-
   margin: ${(props) => props.margin || "unset"};
 
-  :hover {
-    background-color: ${({ theme }) => theme.color.blue1_50};
-  }
-
   a {
+    width: 100%;
+
+    padding: 0.5rem 1rem;
+
     text-decoration: none;
 
     font-weight: bold;
 
+    border-radius: 0.5rem;
+
     color: ${({ theme }) => theme.color.white1};
+    background-color: ${({ theme }) => theme.color.blue1};
+
+    :hover {
+      background-color: ${({ theme }) => theme.color.black1};
+    }
   }
 `;
